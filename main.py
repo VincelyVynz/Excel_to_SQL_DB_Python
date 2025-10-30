c667711cc2977c30b9743e21fd85f47cca8640f7
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-10-30 15:35:54 +0530

    Read and add a table to database

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 #Database URL
 from database_url import SQLALCHEMY_DATABASE_URL, excel_file_path
 
-#File location
-excel_file = excel_file_path
-
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-df = pd.read_excel(excel_file, index_col=None)
-print(df.to_string(index=False))
+df = pd.read_excel(excel_file_path, index_col=None)
+
+sheet_name = pd.ExcelFile(excel_file_path).sheet_names[0]
+
+df.to_sql(sheet_name, engine, if_exists="append", index=False)
+print(f"Table '{sheet_name}' created and all rows inserted.")
